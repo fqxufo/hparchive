7316e7e3c2279232ab742c1d1b1b5962f7bd5053
fqxufo <fqxufo@gmail.com>
2019-05-12 19:59:41 +0800

    生成本地目录fav.html,多页的帖子会被全部下载

--- hparchive.py
+++ hparchive.py
@@ -3,6 +3,7 @@ from requests_html import HTMLSession
 from tqdm import tqdm
 import os
 import time
+import re
 
 
 if not os.path.exists('./hparchive'):
@@ -13,6 +14,8 @@ username = input('用户名： ')
 pwd = input('密码: ')
 
 data = {'username': username, 'password': pwd}
+
+
 
 useragent = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_4) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.1 Safari/605.1.15'}
@@ -34,6 +37,7 @@ def getfav(page=1):
             str(page)
 
     fav = hpsession.get(favurl)
+    
     tbodysel = '#wrap > div.main > div > div.threadlist.datalist > form > table > tbody'
 
     tbody = fav.html.find(tbodysel, first=True)
@@ -53,15 +57,47 @@ def getfav(page=1):
         return maxpage
 
 
+
+def genTOC():
+    tocs = ''
+    for i in favlist:
+        tocs = tocs + '<a href="./hparchive/' + str(i) +'-1.html" target="_blank">' + favlist[i] + '</a><br>' + '\n'
+    
+    with open('fav.html','w') as f:
+        f.write(tocs) 
+        
+
+
+
 # 下载tid对应的帖子html
-def savethread(tid):
+def savethread(tid,page=1):
     rawurl = 'https://www.hi-pda.com/forum/viewthread.php?tid='
-    threadurl = rawurl + str(tid)
+    if (page == 1):
+        
+        threadurl = rawurl + str(tid)
+    
+    else:
+        threadurl = rawurl + str(tid) + '&extra=&page=' + str(page)
+
+
+    # replacestr = 'viewthread.php?tid=' + str(tid) + '&amp;extra=&amp;page=' + str(page)
+    
     r = hpsession.get(threadurl)
+    # modhtml = r.html.html.replace(replacestr,str(tid) + '-' + str(page) + 'html')
+    r1 = 'viewthread.php\?tid=' + tid + '&amp;extra=&amp;page=(\d+)'
+    r2 = tid + r'-\1.html'
+    modhtml = re.sub(r1,r2,r.html.html)
     if (r.status_code != 200):
         print(favlist[tid])
-    with open('./hparchive/' + str(tid) + '.html','wb') as f:
-        f.write(r.content)
+    with open('./hparchive/' + str(tid) + '-' + str(page) + '.html','w',encoding='gb18030') as f:
+        f.write(modhtml)
+    
+    hasnextpage = r.html.find('a.next')
+    time.sleep(0.1)
+    if (len(hasnextpage) > 1 ):
+        savethread(tid,page=page+1)
+
+    
 
 
 
@@ -74,7 +110,10 @@ if maxpagenum > 1:
 
 
 print('一共' + str(len(favlist)) + '个收藏贴')
+genTOC()
+
+
 
 for tid in tqdm(favlist):
     savethread(tid)
-    time.sleep(0.5)
+    time.sleep(0.3)
